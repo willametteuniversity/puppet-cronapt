5524e89bcfef207d7cc7f538e14839144d4f3de8
Thomas Sutton <me@thomas-sutton.id.au>
2011-11-14 10:56:20 +0800

    cron-apt: configure when and who to email
    Adds basic control over the email sent by cron-apt with Puppet control
    over the MAILTO and MAILON configuration variables.

--- manifests/cron.pp
+++ manifests/cron.pp
@@ -5,15 +5,21 @@
 #
 # Parameters:
 #   [*actions*] - list of cron-apt actions to enable.
+#   [*mail*]    - where to send email notifications.
+#   [*mail_on*] - when to send email notifications.
 #
 # Sample Usage:
 #
 #   class { 'apt::cron' :
-#       actions => ['update', 'notify']
+#       actions => ['update', 'notify'],
+#       mail => 'root',
+#       mail_on => 'error',
 #   }
 #
 class apt::cron (
-	$actions = $apt::params::cron_actions
+	$actions = $apt::params::cron_actions,
+	$mail = $apt::params::cron_mail,
+	$mail_on = $apt::params::cron_mail_on
 ) inherits apt::params {
 
 	#
@@ -21,6 +27,26 @@ class apt::cron (
 	#
 	package { "cron-apt" :
 		ensure => installed
+	}
+
+	#
+	# Configure
+	#
+	$mail_on_value = $mail_on ? {
+		'error'   => 'error',
+		'upgrade' => 'upgrade',
+		'changes' => 'changes',
+		'output'  => 'output',
+		'always'  => 'always',
+		default   => 'always',
+	}
+	augeas { "cron_apt-mail_config" :
+		context => "/files/etc/cron-apt/config",
+		changes => [
+			"set MAILON $mail_on_value",
+			"set MAILTO $mail",
+		],
+		require => Package['cron-apt'],
 	}
 
 	#
--- manifests/params.pp
+++ manifests/params.pp
@@ -1,5 +1,17 @@
 class apt::params {
 
+	# The email address to send mail to.
+	$cron_mail = 'root'
+
+	# When to send email about the cron-apt results.
+	# Value: error   (send mail on error runs)
+	#        upgrade (when packages are upgraded)
+	#        changes (mail when change in output from an action)
+	#        output  (send mail when output is generated)
+	#        always  (always send mail)
+	#                (else never send mail)
+	$cron_mail_on = 'error'
+
 	$cron_actions = ['update', 'notify']
 
 }
