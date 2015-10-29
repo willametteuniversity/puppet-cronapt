86d1bd40162c8888f83074d74e8eb3989bc72332
Mark Niedzielski <mniedzielski@nasuni.com>
2015-10-29 14:21:55 -0400

    Make lint really happy.

--- manifests/configure.pp
+++ manifests/configure.pp
@@ -32,47 +32,47 @@ class cronapt::configure (
         'always'   => 'always',
         default    => 'always',
     }
-    augeas { "cronapt-mail_config" :
-        context    => "/files/etc/cron-apt/config",
-        changes    => [
+    augeas { 'cronapt-mail_config' :
+        context => '/files/etc/cron-apt/config',
+        changes => [
                       "set MAILON ${mail_on_value}",
                       "set MAILTO ${mail}",
                       ],
-        require    => Class["cronapt::install"],
+        require => Class['cronapt::install'],
     }
     
     #
     # Actions
     #
     if ('update' in $actions) {
-        file { "/etc/cron-apt/action.d/0-update" :
-            ensure  => "file",
-            source  => "puppet:///modules/cronapt/update",
-            require => Class["cronapt::install"],
+        file { '/etc/cron-apt/action.d/0-update' :
+            ensure  => 'file',
+            source  => 'puppet:///modules/cronapt/update',
+            require => Class['cronapt::install'],
         }
     }
     
     if ('download' in $actions) {
-        file { "/etc/cron-apt/action.d/3-download" :
-            ensure  => "file",
-            source  => "puppet:///modules/cronapt/download",
-            require => Class["cronapt::install"],
+        file { '/etc/cron-apt/action.d/3-download' :
+            ensure  => 'file',
+            source  => 'puppet:///modules/cronapt/download',
+            require => Class['cronapt::install'],
         }
     }
     
     if ('upgrade' in $actions) {
-        file { "/etc/cron-apt/action.d/5-upgrade" :
-            ensure  => "file",
-            source  => "puppet:///modules/cronapt/upgrade",
-            require => Class["cronapt::install"],
+        file { '/etc/cron-apt/action.d/5-upgrade' :
+            ensure  => 'file',
+            source  => 'puppet:///modules/cronapt/upgrade',
+            require => Class['cronapt::install'],
         }
     }
     
     if ('notify' in $actions) {
-        file { "/etc/cron-apt/action.d/9-notify" :
-            ensure  => "file",
-            source  => "puppet:///modules/cronapt/notify",
-            require => Class["cronapt::install"],
+        file { '/etc/cron-apt/action.d/9-notify' :
+            ensure  => 'file',
+            source  => 'puppet:///modules/cronapt/notify',
+            require => Class['cronapt::install'],
         }
     }
     
