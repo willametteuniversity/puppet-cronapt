b0ad86194d16450f455f810e399af0a075cf1d18
Matthew T Nolley <mnolley@puppetmaster.willamette.edu>
2016-07-25 13:39:30 -0700

    NETSRV-4429 # Ensuring removal of cron action scripts

--- manifests/configure.pp
+++ manifests/configure.pp
@@ -5,7 +5,6 @@
 #
 class cronapt::configure {
 
-    # Configuration
     $mail_on_value = $cronapt::mail_on ? {
         'error'    => 'error',
         'upgrade'  => 'upgrade',
@@ -14,45 +13,62 @@ class cronapt::configure {
         'always'   => 'always',
         default    => 'always',
     }
-    augeas { 'cronapt-mail_config' :
-        context => '/files/etc/cron-apt/config',
-        changes => [
-                      "set MAILON ${mail_on_value}",
-                      "set MAILTO ${cronapt::mail}",
-                      ],
-        require => Class['cronapt::install'],
-    }
 
-    # Actions
-    if ('update' in $cronapt::actions) {
-        file { '/etc/cron-apt/action.d/0-update' :
-            ensure  => 'file',
-            source  => 'puppet:///modules/cronapt/update',
+    if $cronapt::absent {
+
+        # make sure no cron scripts or config are present
+        file { 'remove_etc_cron-apt' :
+            ensure  => 'absent',
+            path    => '/etc/cron-apt',
+            recurse => true,
+            purge   => true,
+            force   => true,
+        }
+
+    }
+    else {
+
+        # custom email settings in the cron-apt config file
+        augeas { 'cronapt-mail_config' :
+            context => '/files/etc/cron-apt/config',
+            changes => [
+                         "set MAILON ${mail_on_value}",
+                         "set MAILTO ${cronapt::mail}",
+                         ],
             require => Class['cronapt::install'],
         }
-    }
-    
-    if ('download' in $cronapt::actions) {
-        file { '/etc/cron-apt/action.d/3-download' :
-            ensure  => 'file',
-            source  => 'puppet:///modules/cronapt/download',
-            require => Class['cronapt::install'],
+
+        # make sure the necessary cron scripts are present.
+        if ('update' in $cronapt::actions) {
+            file { '/etc/cron-apt/action.d/0-update' :
+                ensure  => 'file',
+                source  => 'puppet:///modules/cronapt/update',
+                require => Class['cronapt::install'],
+            }
         }
-    }
     
-    if ('upgrade' in $cronapt::actions) {
-        file { '/etc/cron-apt/action.d/5-upgrade' :
-            ensure  => 'file',
-            source  => 'puppet:///modules/cronapt/upgrade',
-            require => Class['cronapt::install'],
+        if ('download' in $cronapt::actions) {
+            file { '/etc/cron-apt/action.d/3-download' :
+                ensure  => 'file',
+                source  => 'puppet:///modules/cronapt/download',
+                require => Class['cronapt::install'],
+            }
         }
-    }
     
-    if ('notify' in $cronapt::actions) {
-        file { '/etc/cron-apt/action.d/9-notify' :
-            ensure  => 'file',
-            source  => 'puppet:///modules/cronapt/notify',
-            require => Class['cronapt::install'],
+        if ('upgrade' in $cronapt::actions) {
+            file { '/etc/cron-apt/action.d/5-upgrade' :
+                ensure  => 'file',
+                source  => 'puppet:///modules/cronapt/upgrade',
+                require => Class['cronapt::install'],
+            }
+        }
+    
+        if ('notify' in $cronapt::actions) {
+            file { '/etc/cron-apt/action.d/9-notify' :
+                ensure  => 'file',
+                source  => 'puppet:///modules/cronapt/notify',
+                require => Class['cronapt::install'],
+            }
         }
     }
 }
